815d5b4be1c33b0e00cd5b806518c8a2fcbd7245
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-06-30 10:20:01 +0400

    done firebase auth connection , and auth component and auth services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
 import { CategoryComponent } from './category/category.component';
 import { MovieCreateComponent } from './movie-create/movie-create.component';
@@ -7,18 +8,19 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
 
 
-const routes:Routes=[
-  {path:'movies',component:MoviesComponent},
-  {path:'movies/category/:categoryId',component:MoviesComponent},
-  {path:'movies/create',component:MovieCreateComponent},
-  {path:'movies/:movieId',component:MovieDetailsComponent},
-  {path:'categories/create',component:CategoryCreateComponent,},
-  
-  {path:'',redirectTo:'movies',pathMatch:'full'},
+const routes: Routes = [
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movies/category/:categoryId', component: MoviesComponent },
+  { path: 'movies/create', component: MovieCreateComponent },
+  { path: 'movies/:movieId', component: MovieDetailsComponent },
+  { path: 'categories/create', component: CategoryCreateComponent, },
+  { path: 'auth', component: AuthComponent },
+
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
 
 ]
 @NgModule({
-  imports:[RouterModule.forRoot(routes)],
-  exports:[RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
